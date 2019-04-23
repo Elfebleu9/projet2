7463cf8231ab034cdd81d68794edc19fc00f71e5
94010-06-11 <94010-06-11@exchange.ad.afpanet>
2019-04-23 15:08:05 +0200

    Avancée formulaire

--- src/Controller/Pjim2Controller.php
+++ src/Controller/Pjim2Controller.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Pjim2Controller extends AbstractController
 {
@@ -32,10 +37,29 @@ class Pjim2Controller extends AbstractController
     }
 
     /**
+    * @Route("/pjim2/new", name="pjim2_create")
+    */
+    public function create(Request $request,ObjectManager $manager){
+        $article= new article();
+
+        $form = $this->createFormBuilder($article)
+                ->add('Titre',TextType::class,['attr'=>['placeholder'=>"Titre de l'article"]])
+                ->add('Contenu', TextareaType::class, ['attr'=>['placeholder'=>"Contenu de l'article"]])
+                ->add('Image', TextType::class,['attr'=>['placeholder'=>"Image de l'article"]])
+                
+                ->getForm();
+
+        return $this->render('pjim2/create.html.twig',[ 'formArticle'=>$form->createView()]);
+    }
+
+    /**
      * @Route("/pjim2/{id}", name="pjim2_show")
      */
     public function show(Article $article){
        
-            return $this->render('pjim2/show.html.twig', [ 'article'=>$article]);
+        return $this->render('pjim2/show.html.twig', [ 'article'=>$article]);
     }
+
+    
+    
 }
