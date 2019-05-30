d02c1dbe560edba2ae0b9b06b05d79377773eb93
Elfebleu9 <poetic.jm@orange.fr>
2019-05-30 17:07:13 +0200

    templates rendu

--- src/Controller/RenduController.php
+++ src/Controller/RenduController.php
@@ -26,14 +26,6 @@ class RenduController extends AbstractController
     }
 
      /**
-     * @Route("/rendu/reussirachat", name="reussite")
-     */
-    public function reussite1(){
-
-        return $this->render('rendu/reussir.html.twig');
-    }
-
-     /**
      * @Route("/rendu/investissement", name="invest")
      */
     public function investissement(){
@@ -49,19 +41,123 @@ class RenduController extends AbstractController
         return $this->render('rendu/conseil.html.twig');
     }
 
-    //  /**
-    //  * @Route("/rendu/reussirachat", name="reussite")
-    //  */
-    // public function reussite4(){
+     /**
+     * @Route("/rendu/simulation", name="simule")
+     */
+    public function simulation(){
 
-    //     return $this->render('rendu/reussir.html.twig');
-    // }
+        return $this->render('rendu/simule.html.twig');
+    }
 
-    //  /**
-    //  * @Route("/rendu/reussirachat", name="reussite")
-    //  */
-    // public function reussite5(){
+     /**
+     * @Route("/rendu/tauxzero", name="taux")
+     */
+    public function tauxzero(){
 
-    //     return $this->render('rendu/reussir.html.twig');
-    // }
+        return $this->render('rendu/taux.html.twig');
+    }
+
+     /**
+     * @Route("/rendu/deroulement", name="deroul")
+     */
+    public function deroul(){
+
+        return $this->render('rendu/deroult.html.twig');
+    }
+
+     /**
+     * @Route("/rendu/aides", name="aides")
+     */
+    public function aides(){
+
+        return $this->render('rendu/aides.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/passage", name="passage")
+     */
+    public function passage(){
+
+        return $this->render('rendu/passage.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/guide", name="guide")
+     */
+    public function guide(){
+
+        return $this->render('rendu/guide.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/maitrise", name="etape")
+     */
+    public function maitrise(){
+
+        return $this->render('rendu/etape.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/lecture", name="plan")
+     */
+    public function lireplan(){
+
+        return $this->render('rendu/lireplan.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/comprehension", name="7cles")
+     */
+    public function comprehension(){
+
+        return $this->render('rendu/7cles.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/verification", name="5points")
+     */
+    public function verif(){
+
+        return $this->render('rendu/verif.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/nextreserve", name="next")
+     */
+    public function nextres(){
+
+        return $this->render('rendu/next.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/remisecles", name="cles")
+     */
+    public function remise(){
+
+        return $this->render('rendu/remise.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/garanties", name="garant")
+     */
+    public function garant(){
+
+        return $this->render('rendu/garanties.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/checklist", name="check")
+     */
+    public function checklist(){
+
+        return $this->render('rendu/check.html.twig');
+    }
+
+      /**
+     * @Route("/rendu/astuces", name="astuces")
+     */
+    public function astuces(){
+
+        return $this->render('rendu/astuces.html.twig');
+    }
 }
