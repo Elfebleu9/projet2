d0a27c3c0be3b93e72ae9e30047f5f59e8d05135
Elfebleu9 <poetic.jm@orange.fr>
2019-06-06 10:13:37 +0200

    Arrangt templ

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -36,7 +36,7 @@ class SecurityController extends AbstractController
 
            
 
-            return $this->redirectToRoute('confirmation');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('security/registration.html.twig',[
